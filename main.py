e4c38158c0e396ff2048e7fe37905b888477a192
unknown <sonic-sonic99@mail.ru>
2020-02-29 22:23:35 +0300

    added Doc

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from neuron import Neuron
 lst = [[0, 0], [0, 1], [1, 0], [1, 1]]
 lst_not = [[1], [0]]
 
-neuron_and = Neuron(1, -3/2)
-neuron_or = Neuron(1, -1/2)
-neuron_not = Neuron(-1, 1)
-
-# or line in data2:
-#     print(line, "\t", and_n.compute_with_bias(line),
-#           "\t", or_n.compute_with_bias(line))
+neuron_and = Neuron([1, 1], -3/2)
+neuron_or = Neuron([1, 1], -1/2)
+neuron_not = Neuron([-1], 1)
 print("Таблица истинности на смещении\nTable\tAND\tOR")
 for i in lst:
     print(i, "\t", neuron_and.compute_with_bias(i), "\t", neuron_or.compute_with_bias(i))
 print("\nTable\tNOT")
+
 for i in lst_not:
     print(i, "\t", neuron_not.compute_with_bias(i))
 
 print("\n\nТаблица истинности через пограничные значения\nTable\tAND\tOR")
+
 for i in lst:
     print(i, "\t", neuron_and.calc_with_threshold(i),
           "\t", neuron_or.calc_with_threshold(i))
@@ -25,9 +23,3 @@ for i in lst:
 print("\nTable\tNOT")
 for i in lst_not:
     print(i, "\t", neuron_not.calc_with_threshold(i))
-    # print(neuron_or.compute_with_bias(i))
-    # print(neuron_and.calc_with_threshold(i))
-    # print(neuron_or.calc_with_threshold(i))
-# for i in lst_not:
-    # print(neuron_not.compute_with_bias(i))
-    # print(neuron_not.calc_with_threshold(i))
--- neuron.py
+++ neuron.py
@@ -1,16 +1,34 @@
+import numpy as np
 class Neuron:
+    """
+    Класс нейрона или перцептрона основанного на методах активации через пограничное значение и смещения
+    """
     def __init__(self, weigth, bias):
-        self.weigth = weigth
+        """
+        Метод инициализации объекта класса
+
+        :param weight: list(list()) 
+        :param bias: Значение смещения
+        :type bias: inf, float
+        """
+        self.weigth = np.array(weigth)
         self.bias = bias
 
     def compute_with_bias(self, x):
-        res = 0
-        for i in x:
-            res = res + (i * self.weigth)
-        return 1 if res + self.bias > 0 else 0
+        """
+        Метод вычисления результата через смещение, а так же активация функции.
+
+        Для получения результата происходит перемножение матрицы весов каждого из входящих значений
+        на матрицу входных данных. К получившейся матрице добавляется значение смещения.
+        """
+        return 1 if np.dot(self.weigth, np.array(x)) + self.bias > 0 else 0
 
     def calc_with_threshold(self, x):
-        res = 0
-        for i in x:
-            res = res + (i * self.weigth)
-        return 1 if res > -self.bias else 0
+        """
+        Метод вычисления результата через пограничное значение, а так же активация функции.
+
+        Для получения результата происходит перемножение матрицы весов каждого из входящих значений
+        на матрицу входных данных. Получившаяся матрица сравнивается со пограничным значением равным
+        значению смещения взятым с противопложным знаком
+        """
+        return 1 if np.dot(self.weigth, np.array(x)) > -self.bias else 0
